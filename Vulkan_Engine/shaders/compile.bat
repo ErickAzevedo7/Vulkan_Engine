0c23399c2a9d6fdb7e60f640d154414d4ecac13b
Erick Azevedo <erick.azevedo7@hotmail.com>
2025-11-05 21:08:23 -0300

    feat: Add post-processing shaders and update compile script

--- Vulkan_Engine/shaders/compile.bat
+++ Vulkan_Engine/shaders/compile.bat
@@ -1,2 +1,5 @@
 C:\VulkanSDK\1.4.309.0\Bin\glslc.exe shader.vert -o vert.spv
 C:\VulkanSDK\1.4.309.0\Bin\glslc.exe shader.frag -o frag.spv
+
+C:\VulkanSDK\1.4.309.0\Bin\glslc.exe postprocess_edge.vert -o postprocess_edge_vert.spv
+C:\VulkanSDK\1.4.309.0\Bin\glslc.exe postprocess_edge.frag -o postprocess_edge_frag.spv
