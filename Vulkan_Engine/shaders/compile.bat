5cfad0206fc7885adbe4a633225d7672274ad688
Erick Azevedo <erick.azevedo7@hotmail.com>
2025-11-08 09:09:50 -0300

    feat: Update shader compilation and add skybox shaders

--- Vulkan_Engine/shaders/compile.bat
+++ Vulkan_Engine/shaders/compile.bat
@@ -3,3 +3,6 @@ C:\VulkanSDK\1.4.309.0\Bin\glslc.exe shader.frag -o frag.spv
 
 C:\VulkanSDK\1.4.309.0\Bin\glslc.exe postprocess_edge.vert -o postprocess_edge_vert.spv
 C:\VulkanSDK\1.4.309.0\Bin\glslc.exe postprocess_edge.frag -o postprocess_edge_frag.spv
+
+C:\VulkanSDK\1.4.309.0\Bin\glslc.exe skybox.vert -o skybox.vert.spv
+C:\VulkanSDK\1.4.309.0\Bin\glslc.exe skybox.frag -o skybox.frag.spv
